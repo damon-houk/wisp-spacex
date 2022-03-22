e16cb755f6d068ff7881449dfd8958df3feb511c
Damon Houk <damon@Damons-MBP.home>
2022-03-22 13:30:38 -0700

    Clean up for merge.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import SpaceXProvider from './components/SpaceXProvider';
@@ -9,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>SpaceX Launches</h1>
+          <img/><h1>SpaceX Launches</h1>
       </header>
       <SpaceXProvider>
         <LaunchGrid />
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,11 +5,6 @@ import {muiTheme} from 'storybook-addon-material-ui';
 
 import {withStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
 
 const action = msg => () => console.log(msg);
 
